5b0484f1ca594ec2ce5e472c37c0bfa5037064ee
Muinul-Islam <muinul1022@gmail.com>
2023-08-31 20:28:03 +0600

    navbar and buttons added

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('assignments');
+const loadData = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+  const data = await res.json();
+  const allCategory = data.data;
+
+  allCategory.forEach(category => {
+    console.log(category);
+    const btnContainer = document.getElementById('btn-container');
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <button class="px-3 py-1 bg-gray-300 text-xl">
+          ${category.category}
+        </button>
+    `
+    btnContainer.appendChild(div);
+  })
+
+};
+
+
+
+loadData();
