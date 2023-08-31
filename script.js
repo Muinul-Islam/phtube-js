5d3cadda780e1271a276e0c20d77b099dcaca305
Muinul-Islam <muinul1022@gmail.com>
2023-09-01 03:29:10 +0600

    time added

--- script.js
+++ script.js
@@ -22,26 +22,48 @@ const loadCategory = async (categoryId) => {
   const data = await res.json();
   const allCategory = data?.data;
   const cardContainer = document.getElementById('card-container')
+  cardContainer.innerHTML = '';
+
+  if (allCategory.length === 0) {
+    const div = document.createElement('div');
+    noContentDiv.innerHTML = `
+    <img src="./img/icon.jpg"/>
+    `;
+    cardContainer.appendChild(div);
+    return
+  }
 
   allCategory?.forEach(category => {
-    console.log(category);
     const div = document.createElement('div');
+    const timeSec = category?.others?.posted_date;
+
+    let timeField = '';
+    if (timeSec !== null) {
+      const timeHours = Math.floor(timeSec / 3600);
+      const remainingTime = timeSec % 3600;
+      const timeMins = Math.floor(remainingTime / 60);
+      timeField = `${timeHours}hrs ${timeMins}min ago`;
+    }
+
     div.innerHTML = `
-          <div class="card bg-base-100 shadow-xl">
-          <figure><img class="h-[350px]" src="${category?.thumbnail}" alt="images" /></figure>
+      <div class="card bg-base-100 shadow-xl">
+          <div class="relative">
+           <img class="h-[350px] rounded-xl" src="${category?.thumbnail}" alt="images" />
+           ${timeSec !== '' ? `<small class="absolute bottom-0 right-0 m-2 p-1 rounded-lg mt-[-30px] bg-black text-white">${timeField}</small>` : ' '}
+          </div >
           <div class="card-body">
-           <div class="flex items-center gap-8">
-            <img class="w-[48px] h-[48px] rounded-full" src="${category?.authors[0]?.profile_picture}" alt="images"/>
-            <h3 class="text-xl font-semibold">${category?.title}</h3>
-           </div>
+            <div class="flex items-center gap-8">
+              <img class="w-[48px] h-[48px] rounded-full" src="${category?.authors[0]?.profile_picture}" alt="images" />
+              <h3 class="text-xl font-semibold">${category?.title}</h3>
+            </div>
             <div class="flex w-9/12">
-            <p class="ml-20">${category?.authors[0].profile_name}</p>
-            <img src="./img/blueTick.jpg" alt="images"/>
+              <p class="ml-20">${category?.authors[0].profile_name}</p>
+              ${category?.authors[0]?.verified === true ? `<img src="./img/blueTick.jpg" alt="images" />` : ' '}
             </div>
             <p class="ml-20">${category?.others?.views} views</p>
           </div>
-        </div>
-    `;
+      </div >
+  `;
 
     cardContainer.appendChild(div)
   });
@@ -50,6 +72,7 @@ const loadCategory = async (categoryId) => {
 
 loadData();
 
+loadCategory("1000");
 
 const blog = () => {
   window.location.href = 'blog.html';
